b756f72837e1a74b7627b641f60aa7eb2b70b590
Alex Kozack <cawa-93@users.noreply.github.com>
2021-08-12 12:09:11 +0300

    Кэширование всех сетевых запросов в течении одной сесии

--- getFranchiseById.js
+++ getFranchiseById.js
@@ -1,25 +1,5 @@
 import {loadAnime} from './shiki-api/loadAnime.js';
 
-
-/**
- *
- * @type {Map<number, string>}
- */
-const cache = new Map;
-
-
 export async function getFranchiseById(id) {
-	const franchise = cache.get(id);
-
-	if (franchise) {
-		return franchise;
-	}
-
-	const anime = await loadAnime(id);
-
-	if (anime.franchise) {
-		cache.set(id, anime.franchise);
-	}
-
-	return anime.franchise;
+	return loadAnime(id).then(anime => anime.franchise);
 }
--- shiki-api/call.js
+++ shiki-api/call.js
@@ -3,7 +3,7 @@ import * as https from 'https';
 
 /**
  *
- * @param {string|URL} options
+ * @param {string|URL} url
  * @return {Promise<unknown>}
  */
 function request(url) {
@@ -24,15 +24,32 @@ function request(url) {
 
 
 /**
- *
+ * Предотвращает повторные однотипные запросы к серверу в пределах одной сессии
+ * @type {Map<string, any>}
+ */
+const runtimeCache = new Map
+
+/**
+ * Выполняет засетевой запрос к Шикимори
+ * Повторяет запрос если возникла ошибка превышения лимита RPS или RPM
+ * Все ответы кэшируются -- повторный запрос к тому же апи не будет отправляться на сервер
  * @param {string} input
  * @return {Promise<any>}
  */
 export function call(input) {
+
+	const cachedResponse = runtimeCache.get(input)
+
+	if (cachedResponse) {
+		return Promise.resolve(cachedResponse)
+	}
+
 	return request(`https://shikimori.one/api/${input}`)
 		.then(({status, body}) => {
 			if (status === 200) {
-				return JSON.parse(body)
+				const responseData = JSON.parse(body)
+				runtimeCache.set(input, responseData)
+				return responseData
 			} else if (status === 429) {
 				console.log(`Api request failed with status code ${status}. Retrying ...`);
 				return new Promise(r => setTimeout(r, 1000 + 1000 * Math.random())).then(() => call(input))
